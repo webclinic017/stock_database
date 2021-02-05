e6dcbee46322004d7c182bbf6db6164ce632aaa4
Nick <75746467+nicholasbrodeur@users.noreply.github.com>
2021-02-05 14:30:18 -0500

    Update add_companies.py
    Refined code and improved user experience

--- add_companies.py
+++ add_companies.py
@@ -3,28 +3,20 @@ author: Nicholas Brodeur
 
 created on 2021-01-14
 
-This file retrieves publicly traded stocks and basic information about them from the Financial Modeling Prep API and stores them into a SQLite database. Once the program 
-is ran, the user has two options: they can enter a specific stock ticker to add/update that stock to the database, or they can press enter to randomly add a select 
-number of new stocks. If you want to run this on your computer, you need to download SQLite and get an API key from https://financialmodelingprep.com/developer/docs/ . 
-FMP can be used for free, but they only allow 250 calls per day, so if you are planning on making real use of their API, it is recommended that you purchase one of their
-plans (unlimited requests can be purchased for only $19/mo with their monthly plan).
+This file retrieves publicly traded stocks and basic information about them from the Financial Modeling Prep API and 
+stores them into a SQLite database.
 
-If you have any suggestions for improvement, I would love the feedback. Hope this is helpful!
 '''
 
 from urllib.request import urlopen
 import json
-import sqlite3
-import time
 
 def get_jsonparsed_data(url):
     """
     Receive the content of ``url``, parse it as JSON and return the object.
-
     Parameters
     ----------
     url : str
-
     Returns
     -------
     dict
@@ -34,52 +26,15 @@ def get_jsonparsed_data(url):
     return json.loads(data)
 
 
-# Create an api_key global variable
-api_key = "ENTER_API_KEY_HERE"
-api_key = "apikey=" + api_key
 
+import sqlite3
+from fmp_stock import fmp_stock
+import time
 
-class fmp_stock:
-    def __init__(self, ticker):
-        self.ticker = ticker
-        self.profile = None
-        self.quote = None
-        self.bal_s = None
-        self.inc_s = None
-        self.cf = None
-
-    def build_url(self, retr='profile'):
-        main = 'https://financialmodelingprep.com/api/v3/'
-        mid = {'profile': 'profile/',
-        	     'quote': 'quote/',
-        	     'bal_s': 'balance-sheet-statement/', 
-               'inc_s': 'income-statement/', 
-               'cf': 'cash-flow-statement/'}
-
-        if retr == 'profile' or retr == 'quote':
-            return str(main+mid[retr]+self.ticker+"?"+api_key)
-        elif retr == 'bal_s' or retr == 'inc_s' or retr == 'cf':
-            return str(main+mid[retr]+self.ticker+"?limit=120&"+api_key)
-        else:
-            print("Error building url")
-            return None
-
-    def pull_profile(self):
-    	self.profile = get_jsonparsed_data(self.build_url())
-
-    def pull_quote(self):
-    	self.profile = get_jsonparsed_data(self.build_url())
-
-    def pull_finc_stmts(self):
-        self.bal_s = get_jsonparsed_data(self.build_url(retr='bal_s'))
-        self.inc_s = get_jsonparsed_data(self.build_url(retr='inc_s'))
-        self.cf = get_jsonparsed_data(self.build_url(retr='cf')) 
-
-
-def add_to_database(ticker):	
+def add_to_database(ticker, api_key):	
 	
 	# Create a stock object
-	stock = fmp_stock(ticker)
+	stock = fmp_stock(ticker, api_key)
 
 	# Pull JSON data from the FMP API
 	print('\nPulling', ticker, 'profile data...')
@@ -119,29 +74,31 @@ def add_to_database(ticker):
 
 
 
+
+
+
 ### MAIN CODE ###
 
+# Open SQLite database
 conn = sqlite3.connect('stocks.sqlite')
 cur = conn.cursor()
 
+# Initialize tables
 cur.executescript('''
 	CREATE TABLE IF NOT EXISTS Sector (
 		id 			INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 		name 		TEXT UNIQUE
 	);
-
 	CREATE TABLE IF NOT EXISTS Industry (
 		id 				INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 		sector_id		INTEGER,
 		name 			TEXT UNIQUE
 	);
-
 	CREATE TABLE IF NOT EXISTS Exchange (
 		id 				INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 		short_name 		TEXT UNIQUE,
 		long_name		TEXT
 	);
-
 	CREATE TABLE IF NOT EXISTS Company (
 		id				INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 		industry_id		INTEGER,
@@ -156,62 +113,78 @@ cur.executescript('''
 	);
 ''')
 
-symbols_list = None
+
+
+# Enter your FMP api key
+key = "ENTER_API_KEY_HERE" 
+
+if key == None:
+	key = input('\nPlease enter your api key. \n> ')
+else:
+	key = "apikey=" + key
+
+	
+# Initialize a variable for ticker symbols to store FMP data for database autofill feature
+symbols_json = None
+
 
 while True:
-	ui = input('Enter a ticker (EX. AAPL) to add/update a specific stock to the database, press enter for database autofill, or \'quit\' to quit... ')
+	# Prompt user to add a specific stock, autofill, or quit
+	ui = input('\nEnter a ticker (EX. AAPL), press enter to autofill, or type \'quit\' to quit... \n> ')
 
+
+	# Autofill
 	if len(ui) < 1:
 		
-		# Pull the symbols list from FMP
-		if symbols_list is None:
-			symbols_list = get_jsonparsed_data('https://financialmodelingprep.com/api/v3/stock/list?' + api_key)
+		# Prompt user to enter the number of stocks they want to pull
+		ui_stock_count = input('\nHow many stocks would you like to pull? (enter \'cancel\' to cancel) \n> ')
+		if ui_stock_count == 'cancel':
+			continue
+
+		# Pull a list of ticker symbols from FMP
+		if symbols_json is None:
+			symbols_json = get_jsonparsed_data('https://financialmodelingprep.com/api/v3/stock/list?' + key)
 		
-		# Find where the code left off 
+		# Find the first ticker symbol from FMP's list that the database hasn't logged
 		start_index = 0
 		while True:
-			cur.execute('SELECT ticker FROM Company WHERE ticker = ? ', ( symbols_list[start_index]['symbol'], ))
+			cur.execute('SELECT ticker FROM Company WHERE ticker = ? ', ( symbols_json[start_index]['symbol'], ))
 			if cur.fetchone() is None:
 				break
 			start_index += 1
 		
-		# Ask the user how many stocks to pull
-		ui_stock_count = input('\nHow many stocks would you like to pull? ')
-		# Initialize a list for tickers
-		tickers = []
+		
+		# Initialize a list for ticker symbols
+		symbols_list = []
 		# Build the list starting with the first missing ticker from the FMP stock list
 		for i in range(int(ui_stock_count)):
-			tickers.append(symbols_list[i+start_index]['symbol'])
+			symbols_list.append(symbols_json[i+start_index]['symbol'])
 
-		# Loop through the tickers, adding unfamiliar tickers to the database
+		# Loop through the ticker symbols, adding unfamiliar tickers to the database
 		count = 0
-		for ticker in tickers:
+		for ticker in symbols_list:
 			
+			# Skip stocks that are already in database; add those that aren't
 			cur.execute('SELECT id FROM Company WHERE ticker = ? ', ( ticker, ))
 			if cur.fetchone() is not None:
 				print(ticker, 'already exists in database')
-				continue
-			
-			add_to_database(ticker)
+			else:
+				add_to_database(ticker, key)
 
-			# Delay program every 30 stocks to avoid overwhelming FMP servers
+			# Delay program every 30 stocks
+			count += 1
 			if (count % 30) == 0:
 				print('\nsleeping...\n')
-				time.sleep(4)
-			count += 1
+				time.sleep(2)
 
 	else:
 		# End program
 		if ui == 'quit':
 			break
 
-		# Attempt to add user input to database
+		# Enter user-input stock to database
 		try:
-			add_to_database(ui)
+			add_to_database(ui, key)
 		except Exception as err:
 			print('Failed to retrieve or enter', ui +'.', err)
 			continue
-
-
-
-
